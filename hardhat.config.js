fa986d613badf3ed117722ad88c980710de60e49
joeljosjoseph <joeljosjoseph12@gmail.com>
2023-01-08 17:52:47 +0530

    Completed Deposit to Aave

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,6 +12,7 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY || "00xprivate";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "00xetherscan";
 const COINMARKETCAP_API_KEY =
   process.env.COINMARKETCAP_API_KEY || "00xcoinmarketcap";
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL;
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -19,6 +20,9 @@ module.exports = {
     hardhat: {
       chainId: 31337,
       blockConfirmations: 1,
+      forking: {
+        url: MAINNET_RPC_URL,
+      },
     },
     goerli: {
       chainId: 5,
@@ -52,7 +56,19 @@ module.exports = {
     outputFile: "gas-report.txt",
     noColors: true,
   },
-  solidity: "0.4.19",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.8",
+      },
+      {
+        version: "0.6.12",
+      },
+      {
+        version: "0.4.19",
+      },
+    ],
+  },
   namedAccounts: {
     deployer: {
       default: 0,
